de85293ec47a2ce36ac3f84fce419188706dfb15
joshbatley <jbatley12@gmail.com>
2020-01-08 11:03:11 +0000

    ci: update CI to include linting and build checks

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Start: React.FC = () => {
+const Start: React.FC = () => (
+  <>
+    <h1>Pursuit for Trivia</h1>
+    <Link to="/mode-selector">Start</Link>
+  </>
+);
 
-  return (
-    <>
-      <h1>Pursuit for Trivia</h1>
-      <Link to="/mode-selector">Start</Link>
-    </>
-  );
-};
 export default Start;
