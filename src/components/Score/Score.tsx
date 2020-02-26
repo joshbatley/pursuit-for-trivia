297f8f68d4fd4587f3a0494c4c7acef854857945
joshbatley <jbatley12@gmail.com>
2020-02-26 11:03:21 +0000

    feat: added score styling

--- src/components/Score/Score.tsx
+++ src/components/Score/Score.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import styles from './styles.module.css';
 
 interface Props {
   score: number;
 }
 
-const Score: React.FC<Props> = ({ score }: Props) => (
-  <div>{score}</div>
-);
+const Score: React.FC<Props> = ({ score }: Props) => {
+  let normalizeScore = '000000'.substring(0, 6 - score.toString().length) + score;
+  return (
+    <div className={styles.score}>{normalizeScore}</div>
+  );
+};
 
 export default Score;
